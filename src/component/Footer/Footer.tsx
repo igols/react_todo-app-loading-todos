3a7bf962a6533a79a98f52685e565b6cae29e965
Igor <igor.olshanskyi@gmail.com>
2025-03-22 12:36:15 +0200

    test_16

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -17,7 +17,7 @@ export const Footer: React.FC<Props> = ({
   return (
     <footer className="todoapp__footer" data-cy="Footer">
       <span className="todo-count" data-cy="TodosCounter">
-        {todos.length}
+        {todos.filter(todo => !todo.completed).length} items left
       </span>
 
       {/*+ Active link should have the 'selected' class */}
